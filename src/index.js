75643f11727860a24af8071d4e4881c8879ac4c6
Marina-tech-dot <marina.juriskonsult.gmail.com>
2022-01-31 21:19:55 +0100

    feat: solution for 11 test from 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let config = bracketsConfig.flat()
+  let arrLength = []
+
+  for (let i = 0; i < config.length; i++) {
+    arrLength.push(str.split("").filter(item => item === config[i]).length);
+  }
+
+  let filteredArr = arrLength.filter((item, index) => index === arrLength.indexOf(item))
+
+  return (filteredArr.length % 2 != 0) ? true : false
+
 }
